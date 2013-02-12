ac88c5abdec6c0858bc38356ac364d5ebcaf7637
Branden Coates <branden.c.coates@gmail.com>
2013-02-12 00:10:24 -0500

    Import of latest project files(clone of local)

---
+++ data/lib/version.rb
@@ -0,0 +1,3 @@
+class VirtConsole
+  VERSION = "1.0.4"
+end
---
+++ data/lib/virt_console.rb
@@ -0,0 +1,23 @@
+class VirtConsole
+  require 'serialport'
+  attr_accessor :port, :command
+  def initialize(hash)
+    @path = hash[:path]
+    @command = hash[:command]
+  end
+  def execute
+    sp = SerialPort.open("#{@path}", baud = 9600, data_bits = 8, stop_bits = 1) { |tty|
+      tty.sync = true
+      line = ""
+      Thread.new {
+        tty.write @command + "\015"
+      }
+      until line =~ /^(Execution Completed|Ready|Disk Shrink Completed|Disk Extend Completed)\.$/ do
+        line = (tty.readline.chomp!)
+        print "#{line}\n"
+      end
+    print "\n"
+    }
+  end
+end
+
